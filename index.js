9eae0ac90c87d56566dea5bdd8b6f2f6d0424b59
NoNameA 774 <nonamea774@gmail.com>
2016-10-28 23:46:13 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const interpreter = require('../../codes/piet-interpreter');
+
+const code = [
+  ['lred', 'dblue', 'dgreen', 'lyellow', 'lmagenta', 'dyellow', 'cyan', 'dred', 'black', 'white', 'dred'],
+  ['white', 'white', 'white', 'white', 'white', 'white', 'white', 'dred', 'dred', 'white', 'dred'],
+  ['white', 'dcyan', 'white', 'white', 'white', 'black', 'white', 'yellow', 'black', 'white', 'dred'],
+  ['white', 'dcyan', 'dcyan', 'dcyan', 'dcyan', 'dcyan', 'dyellow', 'yellow', 'white', 'black', 'dred'],
+];
+
+let world = interpreter.create(code, ['20']);
+let str = "";
+
+while(!world.halt) {
+  str += (JSON.stringify(world.env) + "\n");
+  world = interpreter.next(world);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const editor = document.getElementById('editor');
+  const output = document.getElementById('output');
+  output.innerText = str;
+});
