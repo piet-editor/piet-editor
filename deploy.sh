d8cf3d57a2352a4fb3a7f9462eca725395597975
NoNameA 774 <nonamea774@gmail.com>
2016-10-29 02:22:03 +0900

    add gh-pages settings

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -xe
+
+cd dist
+
+git config user.email "nonaroid@nna774.net"
+git config user.name "nonaroid on Travis-CI"
+
+git init
+git add .
+git commit -m "Deploy to GitHub Pages@`date`"
+git push --force --quiet "https://${GH_TOKEN}@github.com/piet-editor/piet-editor.github.io.git" master:master > /dev/null 2>&1
