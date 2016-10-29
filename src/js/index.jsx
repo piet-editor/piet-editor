60abac73b8ecb4bf4f940ac48ff37bdb42d189f7
NoNameA 774 <nonamea774@gmail.com>
2016-10-29 18:06:50 +0900

    remove interpreter test code

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,27 +1,8 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Editor from './editor';
-
-const ReactDOM = require('react-dom');
-const interpreter = require('piet-interpreter');
-
-const code = [
-  ['lred', 'dblue', 'dgreen', 'lyellow', 'lmagenta', 'dyellow', 'cyan', 'dred', 'black', 'white', 'dred'],
-  ['white', 'white', 'white', 'white', 'white', 'white', 'white', 'dred', 'dred', 'white', 'dred'],
-  ['white', 'dcyan', 'white', 'white', 'white', 'black', 'white', 'yellow', 'black', 'white', 'dred'],
-  ['white', 'dcyan', 'dcyan', 'dcyan', 'dcyan', 'dcyan', 'dyellow', 'yellow', 'white', 'black', 'dred'],
-];
-
-let world = interpreter.create(code, ['20']);
-let str = '';
-
-while (!world.halt) {
-  str += `${JSON.stringify(world.env)}\n`;
-  world = interpreter.next(world);
-}
 
 document.addEventListener('DOMContentLoaded', () => {
   const editor = document.getElementById('editor');
-  const output = document.getElementById('output');
-  output.innerText = str;
   ReactDOM.render(<Editor />, editor);
 });
