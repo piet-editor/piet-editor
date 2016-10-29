807f9ded618aba50c21f8a33e93a5154b6b839dd
NoNameA 774 <nonamea774@gmail.com>
2016-10-30 03:48:56 +0900

    定数をconstants.jsに切り出した。

--- src/js/colorPallet.jsx
+++ src/js/colorPallet.jsx
@@ -1,37 +1,5 @@
 import React from 'react';
-
-const colors = [
-  'lred', 'lyellow', 'lgreen', 'lcyan', 'lblue', 'lmagenta',
-  'red', 'yellow', 'green', 'cyan', 'blue', 'magenta',
-  'dred', 'dyellow', 'dgreen', 'dcyan', 'dblue', 'dmagenta',
-  'white', 'black',
-];
-
-const colorCodes = {
-  lred: '#FFC0C0',
-  lyellow: '#FFFFC0',
-  lgreen: '#C0FFC0',
-  lcyan: '#C0FFFF',
-  lblue: '#C0C0FF',
-  lmagenta: '#FFC0FF',
-
-  red: '#FF0000',
-  yellow: '#FFFF00',
-  green: '#00FF00',
-  cyan: '#00FFFF',
-  blue: '#0000FF',
-  magenta: '#FF00FF',
-
-  dred: '#C00000',
-  dyellow: '#C0C000',
-  dgreen: '#00C000',
-  dcyan: '#00C0C0',
-  dblue: '#0000C0',
-  dmagenta: '#C000C0',
-
-  white: '#FFFFFF',
-  black: '#000000',
-};
+import { colors, colorCodes} from './constants';
 
 const commandTable = [
   '*', 'add', 'div', 'great', 'dup', 'in(c)',
