3de19391489309a631b2415bbc25cce02d2000df
NoNameA 774 <nonamea774@gmail.com>
2016-10-30 07:23:00 +0900

    add buttons

--- src/js/interpreter.jsx
+++ src/js/interpreter.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
 
+import interpreter from 'piet-interpreter';
+
 export default class Interpreter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      pause: false,
     };
   }
 
   render() {
     return (
-      <div />
+      <div>
+        <button
+          onClick={this.run}
+        >
+          run
+        </button>
+        <button
+          onClick={this.step}
+        >
+          step
+        </button>
+        {
+          this.state.pause && (
+            <button
+              onClick={this.continue}
+            >
+              continue
+            </button>)
+        }
+      </div>
     );
   }
 }
